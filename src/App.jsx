49da0b0f3c54d183c220f42581309a81d743a86e
Wathisa <wathisa@gmail.com>
2026-04-21 08:54:16 +0700

    update App file

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
+import { useState } from "react";
 import Castle from "./components/castle";
 
 export default function App() {
+  // creating state variables
+  const [question, setQuestion] = useState("");
+  const handleQuestion = (event) => {
+    setQuestion(event.target.value);
+  };
+
   return (
     <div className="pb-80 py-10 gap-y-4 flex flex-col justify-center items-center min-h-screen bg-gray-800 text-white">
       <p className="text-purple-300">
@@ -10,8 +17,8 @@ export default function App() {
         </span>
       </p>
       <textarea
-        value=""
-        onChange=""
+        value={question}
+        onChange={handleQuestion}
         className="bg-white text-black rounded px-2 py-1 "
         placeholder="Type your message here..."
       />
